0c0dd29cb3bcc2c5db4a4f332e72ee7eb13c4ebc
Alexandre Santos <111398880+allesantos@users.noreply.github.com>
2025-02-16 23:21:48 -0300

    Add files via upload

---
+++ 01-Lampada/script.js
@@ -0,0 +1,13 @@
+// Função que acende a lâmpada
+function acendeLampada() {
+    // Altera o atributo 'src' da imagem com o ID 'luz', trocando pela imagem da lâmpada acesa
+    document.getElementById('luz').src = "imagens/lampada-acesa.jpg";
+}
+
+// Função que apaga a lâmpada
+function apagaLampada() {
+    // Altera o atributo 'src' da imagem com o ID 'luz', trocando pela imagem da lâmpada apagada
+    document.getElementById('luz').src = "imagens/lampada-apagada.jpg";
+}
+
+
